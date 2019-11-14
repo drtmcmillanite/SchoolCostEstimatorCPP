eb756eb14a26be4e363659d362d99de8bd6f0c64
Tyson McMillan <tyson.mcmillan@my.tccd.edu>
2019-11-14 12:07:18 +0000

    Added initial project details

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/*
+Dr. Tyson McMillan School Cost Estimator Using C++. Students were tasked with creating a C++ program from an operational Excel Spreasheet. 
+Nov 14, 2019
+*/
+#include <iostream>
+#include <string>
+using namespace std; 
+
+int main() 
+{
+  cout << "Hello World!\n";
+
+  return 0; 
+}
