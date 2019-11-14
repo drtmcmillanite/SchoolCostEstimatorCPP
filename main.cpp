97ca384fb2deebc1f3dcaedfe63d021e5082de7a
Tyson McMillan <tyson.mcmillan@my.tccd.edu>
2019-11-14 13:05:31 +0000

    create keep it simple framework static constants and member variables ft, pt, ht instances in int main()

--- main.cpp
+++ main.cpp
@@ -12,9 +12,49 @@ int main	3 instances of semester ft, pt, ht
 int main has constant	Declare constants for all those constants values, semstersInAYear, hoursPerClass,
 	Ask the user to populate the constants
 */
+
+class Semester
+{
+  private:
+  //systems assumptions constants
+  //static const variables keep their value even after multiple calls
+    static const int hoursPerClass = 3; 
+    static const int monthsInOneSemester = 4;
+    static const int semestersInAYear = 3; 
+    //user changeable settings (need: setters and getters)
+    int hoursTheStudentTakes; 
+    int hoursInTheDegree; 
+    string schoolName; 
+    double costPerCreditHour; 
+    double bookCostsPerClass;
+    double roomAndBoardCosts; 
+    double foodCostsPerMonth; 
+    double travelCostsPerMonth; 
+  public:
+    Semester() //default constructor
+    {
+        //user changeable settings default values
+        hoursTheStudentTakes = 0; //ft 12, pt 9, ht 6
+        hoursInTheDegree = 60; 
+        schoolName = "TCC"; 
+        costPerCreditHour = 64.00; 
+        bookCostsPerClass = 100.00;
+        roomAndBoardCosts = 400.00; 
+        foodCostsPerMonth = 150.00; 
+        travelCostsPerMonth = 90.00; 
+    }
+};
+
 int main() 
 {
-  cout << "Hello World!\n";
+   //instances of the 3 types of semesters
+   Semester ft;
+   Semester pt;
+   Semester ht; 
+
+   static const int hoursAFullTimeStudent = 12;
+    static const int hoursAPartTimeStudent = 9;
+    static const int hoursAHalfTimeStudent = 6;
 
   return 0; 
 }
